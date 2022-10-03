03c54c0ed2640cf78ffe3a41ebc5a526f252d611
pauvretimo <73127408+pauvretimo@users.noreply.github.com>
2022-10-03 16:16:24 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-steamsetup="/home/steamcmd/steamcmd.sh"
+steamsetup="/home/steamcmd/steamcmd.sh +force_install_dir ./starbound +login "
 server="/home/steamcmd/starbound/linux/starbound_server"
 
 if [ "$1" = "setup" ]; then
-  $steamsetup
-else
-  $server
+  "$steamsetup $2 $3 +app_update 211820 +quit"
+fi
+
+if [ "$1" = "update" ]; then
+  "$steamsetup $2 $3 +app_update 211820 validate +quit"
+fi
+
+$server
